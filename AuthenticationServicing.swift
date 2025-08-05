3b0918b2ce7b6e7f76a57d182e6eab619dc38b64
Özge Kurnaz <ozge.k60@hotmail.com>
2025-08-06 00:55:04 +0300

    Apply SRP to AuthenticationService

---
+++ AuthenticationServicing.swift
@@ -0,0 +1,9 @@
+//
+//  AuthenticationServicing.swift
+//  Flash Chat iOS13
+//
+//  Created by özge kurnaz on 5.08.2025.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import Foundation
